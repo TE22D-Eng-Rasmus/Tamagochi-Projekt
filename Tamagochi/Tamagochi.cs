76ec3debc3835fc39b5aeddb8f17f938226d3a2a
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-09-27 12:40:12 +0200

    first commit

---
+++ Tamagochi/Tamagochi.cs
@@ -0,0 +1,23 @@
+public class Tamagochi{
+
+
+    private int hunger;
+
+    private int boredom;
+
+    private  List<string>words;
+
+    private bool isAlive;
+
+    public string name;
+
+    public void feed(){
+
+    }
+
+    public void Hi(){
+
+    }
+
+    public void teach(){}
+}
